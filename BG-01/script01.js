0e6f31a2a804959f6d7cb8e84990f580a0e3c936
NitinDharmesh <nitindharmesh01@gmail.com>
2025-02-19 16:36:47 +0530

    JavaScript

--- BG-01/script01.js
+++ BG-01/script01.js
@@ -153,3 +153,126 @@ console.log(firstFavNumber != secondFavNumber);
 //                        STRINGS;
 // ==============================================================;
 
+let firstName = "Killer ";
+let secondName= "Bunny";
+
+// String concatenation; 
+let fullName = firstName +" " + secondName;
+console.log(fullName);// Killer Bunny;
+
+// Using some methods for the concatenation; 
+let NewName = firstName.concat( secondName);
+console.log(NewName);
+
+// Append; Appending new value to the variable;
+firstName += "Shark";
+console.log(firstName);
+
+// 3.length of string; 
+console.log(firstName.length);
+
+// 4. Cases; 
+console.log(firstName.toLowerCase());
+console.log(firstName.toUpperCase());
+
+// 5. slice;
+console.log(firstName.slice(0,2));// Ki
+
+// 6. split & join; split will split string and join will join the string; 
+console.log(firstName.split(""));
+
+// 7. Includes; check if string some specific word and return output as boolean; 
+console.log(firstName.includes("K")); 
+
+// 8. Trim: it will remove extra space from the string; 
+console.log(firstName.trim());
+
+
+
+// ==================================================================
+//                       Template String
+// =-================================================================
+
+let desc = `this is random 
+text`; // it will include all spaces and everything; 
+
+
+let Firstname = "John"; 
+let Lastname = "cena";
+
+let Fullname = `${Firstname} ${Lastname} something`;
+console.log(Fullname);
+
+
+
+// PracticesFils; 
+
+// 1. create a variable name(favActorFirstName) & store your actor name; 
+let favActorFirstName ="Tom ";
+// 2. create a variable name (favActorLastName) & store last name; 
+let favActorLastName = "Holland"; 
+
+// 3. create a variable name (FULLNAME) & concatenate {favActor Name}; 
+
+let FULLNAME = favActorFirstName.concat(favActorLastName);
+console.log(FULLNAME);
+FULLNAME =`${favActorFirstName} ${favActorLastName}`;
+console.log(FULLNAME);
+
+FULLNAME = favActorFirstName + " " + favActorLastName;
+console.log(FULLNAME);
+
+// 4. create a variable(uppercase) & capitalize fav actor name; 
+ let uppercase = FULLNAME.toUpperCase();
+ console.log(uppercase);
+
+// 5. create a variable name (message) & store 'my favactor and say something about him; 
+
+let message = `${favActorFirstName} ${favActorLastName} is one of my fav actor who plays as spiderMan one of my fav superhero `;
+console.log(message);
+// 6. now append this message to the message variable 1 his best show is silicon valley; 
+message += `his best movie is home coming`;
+// 7. now log everything; 
+console.log(message);
+
+
+
+
+
+
+
+
+// ==================================================================
+//                           Type Conversion;
+// ==================================================================
+
+// STring to Number; 
+
+let money = "50"; 
+// 1. money = parseInt(money); 
+money = parseInt(money);
+
+// 2. money = +money;
+money = +money;
+
+// Money = Number(money);
+money = Number(money);
+
+
+
+// Number to string; 
+ let Money = 50;
+
+//  1. money.toString()
+Money.toString();
+
+// 2.String(Money);
+String(money);
+
+
+// convert string to decimal; 
+let MMoney = "502.555"; 
+
+MMoney = parseFloat(MMoney);
+console.log(MMoney);
+ 
