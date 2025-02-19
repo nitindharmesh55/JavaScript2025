a6991600ce270634d3f2cb0699137cd321ad699c
NitinDharmesh <nitindharmesh01@gmail.com>
2025-02-19 12:00:48 +0530

    variable, maths, operator

---
+++ BG-01/script01.js
@@ -0,0 +1,155 @@
+// 1.Create a variable(Name) and store your name;
+let name = "Nitin";
+
+// 2.  create a variable(goal) and store your goall
+ let goal = "Rich-guy";
+
+//  3. create a variable (gender) and store your gender;
+let gender = "male";
+
+// 4. create a variable (instagram) and store your id;
+let instagram = "@nectorcode";
+
+// Logging all the variable;
+console.log(name);
+console.log(goal);
+console.log(gender);
+console.log(instagram);
+
+
+
+// Primitive Data type : 1. Number;
+let num = 2025.5;// we've stored a number value to a variable;
+
+// Basic Math: we can use manipulate the value like adding or doing other kind of mathematical operations; 
+
+// Addition:
+let num1 = 25;
+let num2 = 25;
+console.log(num1 + num2); // it will add these two values which is stored in two different variable;
+console.log(25 + 25); // we can also do this as well;
+
+// Subtraction;
+let num3 = 50;
+let num4 = 25;
+console.log(num3 - num4);
+console.log(50-25);
+
+// Multiplication;
+let num5 = 12;
+let num6 = 12;
+console.log(num5 * num6);
+
+// Division;
+let num7 = 50;
+let num8 = 2;
+console.log(num7 / num8);
+
+// Remainder;
+let num9 = 64;
+let num10 = 2;
+console.log(num9 % num10);
+
+// Power;
+console.log(2 ** 2);
+
+
+
+// Boolean: it's value that can be true or false on or off;
+let isTrue = true;
+console.log(isTrue);
+
+let isFalse = false;
+console.log(isFalse);
+
+let not = NaN;
+console.log(not)// not a number;
+
+//Falsy value: 1.NULL, 2.Undefined, 3. 0,-0  , 4. NaN; 5."Empty string or quote";
+
+// Truthy value: other than the falsy value everything is true;
+
+
+// Practice: 
+// 1.Create variable name(isprogramminglanguage) store true as value; 
+let isprogramminglanguage = true;
+let ishard = false;
+let favNumber = 25;
+console.log(favNumber + undefined)// it will give you NaN
+
+
+
+// Comparison operator; it will compare two values and the output value will be true or false; 
+
+// Relational operators:
+// Equality Operators:
+
+// Relational Operator: 
+//  > Greater than
+//  < less than;
+// >= greater than equal to; 
+// <= less than equal to; 
+
+let x = 10; 
+let y = 20; 
+
+console.log( x > y) // false;
+console.log(x < y) // True;
+console.log(x >= y) // false;
+console.log(x <= y)// True;
+
+// Equality Operator : 
+// 1. === Strict equality(type + value);
+// 2. !== strict non-equality(type + value);
+// 3. == lose equality operator ;(value);
+// 4. != not equality operator; (value);
+
+console.log(10 === 10);// true;
+console.log( 10 !== 10) // false;
+console.log(10 !== "10")// true;
+console.log(10 == "10");// true;
+console.log(10 != "10")// false
+
+
+
+
+
+
+
+
+
+// Practice start here: 
+
+// 1. create a variable name(firstFavNumber) & store your favorite number; 
+let firstFavNumber = 10;
+
+// 2. create variable name second fav number store fav number; 
+let secondFavNumber = 12; 
+// 3.check if (first number is greater than second);
+console.log( firstFavNumber > secondFavNumber);
+
+// 4. check if first number is less than second number;
+console.log(firstFavNumber < secondFavNumber);
+
+// 5. check if first number is grater than equal to second number ;
+console.log(firstFavNumber >= secondFavNumber);
+
+// 6. check  if first number is less than equal to second number;
+console.log(firstFavNumber <= secondFavNumber);
+
+// 7. check if first number is equal to second number using the strict equality; 
+console.log(firstFavNumber === secondFavNumber);
+
+// 8. check if first number is equal to using lose equality ;
+console.log( firstFavNumber == secondFavNumber);
+// 9. check if first number is not equal to second number using strict non equality;
+console.log(firstFavNumber !== secondFavNumber);
+
+// 10 check if first number is not equal to second number using lose non equality; 
+console.log(firstFavNumber != secondFavNumber);
+
+
+// ==============================================================;
+//                        STRINGS;
+// ==============================================================;
+
